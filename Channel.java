b2519251b8bb8ac47ee4bc89560d4f3b3272b300
UtsavChatterjee1988 <205112037@nitt.edu>
2013-09-27 23:13:49 +0530

    Update Channel.java

--- Channel.java
+++ Channel.java
@@ -5,8 +5,7 @@ public class Channel
 {
     int n;
     boolean flag=false;
-    boolean[] timeup={false,false,false,false};
-    int time_index=-1;
+    boolean timeup=false;
     boolean done=false;
     boolean frame_to_resend=false;
     
@@ -30,13 +29,12 @@ public class Channel
         this.n=n;
         
         do{
+            timeup=false;
+            k=r.nextInt(4);
+            int l = 600*(k+1);
             
-            k=r.nextInt(3);
-            int l = 800*(k+1);
-            
-            System.out.println("Sender : Sending "+n+" over the channel   (Estimated Transmission Delay = "+l+"ms)");
-            Timer tm=new Timer(5000,this);
-            timeup[time_index]=false;
+            System.out.println("Sender : Sending "+n+" over the channel   (Estimated RTT = "+l+"ms)");
+            Timer tm=new Timer(2000,this);
             
             try{
                 tm.t.start();
@@ -46,18 +44,17 @@ public class Channel
                 System.err.println("Method interrupted!");
             }
             
-            if(!timeup[time_index]){
+            if(!timeup){
                 flag=true;
-                notify();
                 
-                /*try{
-                    //tm.t.join();
+                try{
+                    tm.t.join();
                 }catch(InterruptedException e)
                 {
                     System.err.println("Method interrupted!");
                 }
                 
-                notify();*/
+                notify();
                 
             }
             else{
@@ -77,11 +74,17 @@ public class Channel
                 System.err.println("Method interrupted!");
             }
         }
-        k=r.nextInt(2);
-        if(k==0){
-            System.out.println("Receiver : Received "+n+". Sending acknowledgement "+((n+1)%2)+"\n");
+        k=r.nextInt(4);
+        //k=3;
+        if(k==3){
+            this.n=1-this.n; //frame garbled
+        }
+        if(this.n==Receiver.i){    //received as expected
+            System.out.println("Receiver : Received "+Receiver.i+". Sending acknowledgement "+((n+1)%2)+"\n");
+            //Receiver.arr[Receiver.index++]=n;
+            Receiver.i=1-Receiver.i;
         }else{
-            System.out.println("Receiver : Received frame is in error or was not expected. Sending NAK "+n+"\n");
+            System.out.println("Receiver : Received frame is in error or was not expected. Sending NAK "+Receiver.i+"\n");
             frame_to_resend=true;
             Sender.i=1-Sender.i;
         }
